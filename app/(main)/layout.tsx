97ce93861d3a5b110824c267c94c9e342a0b28fb
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2026-03-10 14:38:56 +0530

    wrapping modal in suspense boundary

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,12 +1,15 @@
 import ContactModal from "@/modal/ContactModal";
 import Navbar from "../components/navbar";
+import { Suspense } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Navbar />
       <div>{children}</div>
-      <ContactModal />
+      <Suspense fallback={null}>
+        <ContactModal />
+      </Suspense>
     </div>
   );
 };
