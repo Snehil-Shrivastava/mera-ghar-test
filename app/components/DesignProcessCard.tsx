cf553170d354ea49e02ce02943b9866651d616a8
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2026-03-23 15:11:23 +0530

    text

--- app/components/DesignProcessCard.tsx
+++ app/components/DesignProcessCard.tsx
@@ -16,8 +16,10 @@ const DesignProcessCard = ({
         <Image src={icon} alt="" />
       </div>
       <div>
-        <h3 className="text-2xl text-brand-brown font-bold">{title}</h3>
-        <p className="text-brand-brown pt-2 max-w-70 mx-auto">{description}</p>
+        <h3 className="text-xl text-brand-brown font-bold">{title}</h3>
+        <p className="text-brand-brown pt-2 max-w-70 mx-auto text-base">
+          {description}
+        </p>
       </div>
       {/* <div className="uppercase text-lg font-semibold bg-copper text-cream py-4 px-8">
         <button className="flex gap-2 items-center">
