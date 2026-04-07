5194146f3393844b85789e3d99a6927ed0ed9544
Snehil-Shrivastava <snehil.shrivastava@xvscreations.com>
2026-04-07 15:15:18 +0530

    floating banner svg change

--- app/components/FloatingBanner.tsx
+++ app/components/FloatingBanner.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import freedelivery from "@/public/lifetime_delivery.svg";
+import lifetimeWarranty from "@/public/lifetime_warranty.svg";
 import delivery30days from "@/public/30_day_delivery.svg";
 import easyEMI from "@/public/easy_emi_options.svg";
 import annualService from "@/public/annual_service_program.svg";
@@ -17,18 +17,18 @@ const FloatingBanner = () => {
     // </div>
     <div className="md:flex md:justify-between md:items-center max-md:grid max-md:grid-cols-2 max-md:justify-items-center gap-20 max-md:gap-y-10 max-md:gap-x-0 max-sm:gap-10 pb-10 w-full">
       <div className="w-52.5 max-lg:w-37.5 max-md:w-35 max-sm:w-32">
-        <Image src={freedelivery} alt="" />
+        <Image src={lifetimeWarranty} alt="" />
       </div>
       <div className="w-52.5 max-lg:w-37.5 max-md:w-35 max-sm:w-32 md:hidden">
         <Image src={delivery30days} alt="" />
       </div>
-      <div className="w-40 max-lg:w-25 max-md:w-27 max-sm:w-24">
+      <div className="w-52.5 max-lg:w-37.5 max-md:w-35 max-sm:w-32">
         <Image src={easyEMI} alt="" />
       </div>
-      <div className="w-40 max-lg:w-25 max-md:w-27 max-sm:w-24">
+      <div className="w-52.5 max-lg:w-37.5 max-md:w-35 max-sm:w-32">
         <Image src={annualService} alt="" />
       </div>
-      <div className="w-52.5 max-lg:w-37.5 max-md:w-30 max-sm:w-27 max-md:hidden">
+      <div className="w-52.5 max-lg:w-37.5 max-md:w-35 max-sm:w-32 max-md:hidden">
         <Image src={delivery30days} alt="" />
       </div>
     </div>
